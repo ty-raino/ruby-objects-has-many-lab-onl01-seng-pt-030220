91ee0375a5ea0684c11a23a5436cca86083f3067
Ty Raino <mstymetris@yahoo.com>
2020-03-30 20:34:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
-attr_accessor :title, :artist
+attr_accessor :name, :artist
 
   @@all = []
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
     @@all << self
   end
 
