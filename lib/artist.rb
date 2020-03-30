bc85f6b1ee3baf6c73512132e477b9d4406142b1
Ty Raino <mstymetris@yahoo.com>
2020-03-30 20:34:21 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all
+  end
+
+  def add_song(song_name)
+  song_name.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+attr_accessor :title, :artist
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+end
